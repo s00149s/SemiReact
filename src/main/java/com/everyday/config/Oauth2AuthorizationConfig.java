4d7fde6ed6ce9c2b8437d3d6a345a47f1c41dc81
문길래 <gmoon.au@gmail.com>
2022-02-03 12:58:15 +0900

    OAuth 제거

--- src/main/java/com/everyday/config/Oauth2AuthorizationConfig.java
+++
@@ -1,9 +0,0 @@
-//package com.everyday.config;
-//
-//import org.springframework.context.annotation.Configuration;
-//
-//@Configuration
-//@EnableAuthorizationServer    // OAuth2 권한 서버
-//public class Oauth2AuthorizationConfig extends AuthorizationServerConfigurerAdapter {
-//
-//}
